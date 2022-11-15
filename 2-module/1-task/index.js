702dd024cfc02b53f876dffe08ff9665d742a32e
chikaAS <chika_lsh@mail.ru>
2022-11-15 14:10:28 +0700

    2 модуль 3 задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
   // ваш код...
+let muss=0
+  for (let key in salaries) 
+  {
+  if (Number.isFinite(salaries[key])) muss= muss + salaries[key]
+  }
+  return muss
 }
