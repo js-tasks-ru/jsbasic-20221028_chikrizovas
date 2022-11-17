22939dc0e3369ed1c7de08480d535fbc6c9446df
chikaAS <chika_lsh@mail.ru>
2022-11-17 17:03:20 +0700

    в 5 задание использовал Math.max(...arr),не доконца понял, что это, при возможности расскажите на встрече

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,10 @@
 function filterRange(arr, a, b) {
   // ваш код...
+let max=0
+if (a>b) {max = a, a=b} else {max = b} //max и a=min
+  
+let mass = arr.filter (function(item, index, array){
+  return item<=max && item>=a
+})
+return mass
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,12 @@
 function showSalary(users, age) {
   // ваш код...
+let str=''
+for (let i=0;i<users.length;i++)
+{
+  if (users[i].age<=age)
+  {
+    str=str + users[i].name + ', ' + users[i].balance + '\n'
+  }
+}
+return str.slice(0,[str.length-1])
 }
