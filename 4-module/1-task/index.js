8c7ba756009486b6a381570d4df848c802128e0f
chikaAS <chika_lsh@mail.ru>
2022-11-29 23:15:56 +0700

    4 и 5 модуль

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
   // ваш код...
-}
+  let list=document.createElement('UL')
+  for (let i=0;i<friends.length;i++)
+  {
+    let newLI=document.createElement('LI')
+    newLI.innerHTML=friends[i].firstName + ' ' +friends[i].lastName
+    list.append(newLI)
+  }
+  return list
+  }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
   // ваш код...
+  let Karysel_Inner = document.querySelector(".carousel__inner").offsetWidth
+  let Karysel_Right = document.querySelector(".carousel__arrow_right")
+  let Karysel_Left = document.querySelector(".carousel__arrow_left")
+  let Karysel_SLide = document.querySelectorAll(".carousel__slide").length
+
+  let slaid=0
+
+    sdvig(slaid, Karysel_Right, Karysel_Left, Karysel_SLide)
+     
+    Karysel_Right.addEventListener( "click", () => {
+    slaid++
+    document.querySelector(".carousel__inner").style.transform = `translateX(-${slaid * Karysel_Inner}px)`
+    sdvig(slaid, Karysel_Right, Karysel_Left, Karysel_SLide)
+  })
+
+  Karysel_Left.addEventListener( "click", () => {
+    slaid--
+    document.querySelector(".carousel__inner").style.transform = `translateX(-${slaid * Karysel_Inner}px)`
+    sdvig(slaid, Karysel_Right, Karysel_Left, Karysel_SLide)
+  })
+}
+
+
+function sdvig(slaid, Karysel_Right, Karysel_Left, dKarysel_SLidel) {
+  
+  if (slaid == 0) {
+    Karysel_Left.style.display = 'none'
+  } else {
+    Karysel_Left.style.display = ''
+  }
+
+  if (slaid == dKarysel_SLidel -1) {
+    Karysel_Right.style.display = 'none'
+  } else {
+    Karysel_Right.style.display = ''
+  }
 }
